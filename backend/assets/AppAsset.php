836fdcac01ee0286b4979ba0f2b16a44f15adc12
Ailton Duarte <adidas.coyas@gmail.com>
2019-02-27 13:38:58 -0100

    backend/main.php done

--- backend/assets/AppAsset.php
+++ backend/assets/AppAsset.php
@@ -12,7 +12,7 @@ class AppAsset extends AssetBundle
     public $basePath = '@webroot';
     public $baseUrl = '@web';
     public $css = [
-//        'css/site.css',
+        'css/site.css',
         'bower_components/bootstrap/dist/css/bootstrap.min.css',
         'bower_components/font-awesome/css/font-awesome.min.css',
         'bower_components/Ionicons/css/ionicons.min.css',
